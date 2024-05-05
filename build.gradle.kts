19c8c1802abc39ed3e82fca1ba368a5bdfbf9c8d
RAY <shyrule789@gmail.com>
2024-05-05 11:53:00 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    dependencies {
+        // Hilt
+        //classpath ("com.google.dagger:hilt-android-gradle-plugin:2.47")
+
+        // navArgs
+        //classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.1")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
