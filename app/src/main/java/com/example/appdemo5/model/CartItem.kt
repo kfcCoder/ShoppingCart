dfda2ff8b59399157065371e853ba9c5293c869c
RAY <shyrule789@gmail.com>
2024-05-05 12:03:41 +0800

    add model

---
+++ app/src/main/java/com/example/appdemo5/model/CartItem.kt
@@ -0,0 +1,6 @@
+package com.example.appdemo5.model
+
+data class CartItem(
+    val product: Product,
+    val quantity: Int,
+)
---
+++ app/src/main/java/com/example/appdemo5/model/Product.kt
@@ -0,0 +1,9 @@
+package com.example.appdemo5.model
+
+data class Product(
+    val id: String,
+    val name: String,
+    val price: Int,
+    val isAvailable: Boolean,
+    val imageUrl: String
+)
